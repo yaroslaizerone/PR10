07cac2368c080c2febf8e259ecde272297f1caf3
yaroslaizerone <99257936+yaroslaizerone@users.noreply.github.com>
2022-05-09 22:42:57 +0700

    End Version PR10)

---
+++ app/src/main/java/com/example/pr_10/triedscreen.java
@@ -0,0 +1,14 @@
+package com.example.pr_10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class triedscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_triedscreen);
+    }
+}
